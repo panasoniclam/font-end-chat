47a35c1db6649fb3ca15ffbad93de42bd25c3c69
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-01-29 11:51:26 +0700

    complete webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,20 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+var path    = require('path');
+var hwp     = require('html-webpack-plugin');
+
 module.exports = {
-    entry: path.join(__dirname, 'src/index.js'),
+    entry: path.join(__dirname, '/src/index.js'),
     output: {
-        filename: "index_handle.js",
+        filename: 'build.js',
         path: path.join(__dirname, '/dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['raw-loader']
-
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['css-loader', 'style-loader']
-            },
-            {
-                exclude: /node_modules/,
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                exclude: /node_modules/,
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-        ]
+    module:{
+        rules:[{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src/index.html'),
-    })
+    plugins:[
+        new hwp({template:path.join(__dirname, '/src/index.html')})
     ]
 }
