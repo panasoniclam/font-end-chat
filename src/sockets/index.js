021aa5bea18a09ef54957d17784760ec97d20325
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-01-30 10:02:16 +0700

    ipdate

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,5 +1,5 @@
 const   setupSocket =  (dispatch) =>{
-    const socket = new WebSocket('ws://42.116.254.246:3000/websocket')
-    console.log(socket)
+      const socket = new WebSocket('ws://42.116.254.246:3000/websocket')
+  
 }
 export default setupSocket
